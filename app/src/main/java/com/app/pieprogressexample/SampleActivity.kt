05ac4ef374f3fa6c516075b3adf1199a00c8896e
Magno Ramos <magno23ramos@gmail.com>
2021-03-10 23:24:27 -0300

    improving sample

--- app/src/main/java/com/app/pieprogressexample/SampleActivity.kt
+++ app/src/main/java/com/app/pieprogressexample/SampleActivity.kt
@@ -2,10 +2,24 @@ package com.app.pieprogressexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sample.*
 
 class SampleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sample)
+
+        btn_plus.setOnClickListener { increase() }
+        btn_minus.setOnClickListener { decrease() }
+    }
+
+    private fun increase() = updatePercentage(10f)
+
+    private fun decrease() = updatePercentage(-10f)
+
+    private fun updatePercentage(percentage: Float) {
+        progress1.setPercentage(progress1.currentPercentage + percentage)
+        progress2.setPercentage(progress2.currentPercentage + percentage)
+        progress3.setPercentage(progress3.currentPercentage + percentage)
     }
 }
